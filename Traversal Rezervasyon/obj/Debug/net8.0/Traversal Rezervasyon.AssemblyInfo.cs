bc5baf121b9349896c0e6f7f42527c4562e0cd8e
Furkan Türker <furkanturker.dev@gmail.com>
2025-10-31 00:00:00 +0300

    Layout Düzenlemeleri ve Partial Async

--- Traversal Rezervasyon/obj/Debug/net8.0/Traversal Rezervasyon.AssemblyInfo.cs
+++ Traversal Rezervasyon/obj/Debug/net8.0/Traversal Rezervasyon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674e507aac0c523a43f68482cb44f544b397ba16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dfba458fce4bab1d76dac7e70a5598f65b6f0da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
