7401621deb5fe184738adf3939691426d27e8b2d
Furkan Türker <furkanturker.dev@gmail.com>
2025-10-30 22:58:21 +0300

    DAL işlemleri tamamlandı

---
+++ DataAccessLayer/EntityFramework/EfDestinationDal.cs
@@ -0,0 +1,9 @@
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.EntityFramework;
+
+public class EfDestinationDal :  GenericRepository<Destination>, IDestinationDal
+{
+    
+}
--- DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+700aaf123943630dcccbc31fc44d563cd087fa5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ee2bd28fa7a52074c790d890e0a3f319cf80a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
