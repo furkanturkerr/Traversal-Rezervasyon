5219ab8b3c9b0e03de2064ca9df83408a1ff3843
Furkan Türker <furkanturker.dev@gmail.com>
2025-11-07 00:03:31 +0300

    User Bilgilerinin Profilde Listelenmesi

--- Traversal Rezervasyon/Areas/Member/Controllers/ProfileController.cs
+++ Traversal Rezervasyon/Areas/Member/Controllers/ProfileController.cs
@@ -1,12 +1,29 @@
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Traversal_Rezervasyon.Areas.Member.Models;
 
 namespace Traversal_Rezervasyon.Areas.Member.Controllers;
-
+[Area("Member")]
+[Route("[area]/[controller]/[action]")]
 public class ProfileController : Controller
 {
-    // GET
-    public IActionResult Index()
+    private readonly UserManager<AppUser> _userManager;
+
+    public ProfileController(UserManager<AppUser> userManager)
     {
-        return View();
+        _userManager = userManager;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var values = await _userManager.FindByNameAsync(User.Identity.Name);
+        UserEditViewModel userEditViewModel = new UserEditViewModel();
+        userEditViewModel.Name = values.Name;
+        userEditViewModel.SurName = values.Surname;
+        userEditViewModel.Mail = values.Email;
+        userEditViewModel.PhoneNumber = values.PhoneNumber;
+        return View(userEditViewModel);
     }
 }
---
+++ Traversal Rezervasyon/Areas/Member/Models/UserEditViewModel.cs
@@ -0,0 +1,12 @@
+namespace Traversal_Rezervasyon.Areas.Member.Models;
+
+public class UserEditViewModel
+{
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public string Pasword { get; set; }
+    public string ConfirmPasword { get; set; }
+    public string PhoneNumber { get; set; }
+    public string imageUrl { get; set; }
+    public string Mail { get; set; }
+}
