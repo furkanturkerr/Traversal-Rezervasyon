6a5ccced601c41bcc5a36d0d248ea27d38d840b8
Furkan Türker <furkanturker.dev@gmail.com>
2025-11-09 17:34:43 +0300

    Admin Destination

--- Traversal Rezervasyon/Areas/Admin/Controllers/DestinationController.cs
+++ Traversal Rezervasyon/Areas/Admin/Controllers/DestinationController.cs
@@ -1,3 +1,6 @@
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Traversal_Rezervasyon.Areas.Admin.Controllers;
@@ -5,9 +8,44 @@ namespace Traversal_Rezervasyon.Areas.Admin.Controllers;
 [Area("Admin")]
 public class DestinationController : Controller
 {
-    // GET
+    private DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
     public IActionResult Index()
     {
+        var values = destinationManager.GetAll();
+        return View(values);
+    }
+    
+    [HttpGet]
+    public IActionResult AddDestination()
+    {
         return View();
+    }
+    
+    [HttpPost]
+    public IActionResult AddDestination(Destination destination)
+    {
+        destinationManager.Add(destination);
+        return RedirectToAction("Index");
+    }
+    
+    public IActionResult DeleteDestination(int id)
+    {
+        var value = destinationManager.GetById(id);
+        destinationManager.Delete(value);
+        return RedirectToAction("Index");
+    }
+    
+    [HttpGet]
+    public IActionResult UpdateDestination(int id)
+    {
+        var value = destinationManager.GetById(id);
+        return View(value);
+    }
+    
+    [HttpPost]
+    public IActionResult UpdateDestination(Destination destination)
+    {
+        destinationManager.Edit(destination);
+        return RedirectToAction("Index");
     }
 }
--- Traversal Rezervasyon/obj/Debug/net8.0/Traversal Rezervasyon.AssemblyInfo.cs
+++ Traversal Rezervasyon/obj/Debug/net8.0/Traversal Rezervasyon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd9e3f762d58ca3620b2543d32700e80006abe5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2380c4f817e7b9b41cd04e86394b867061fdd98f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
