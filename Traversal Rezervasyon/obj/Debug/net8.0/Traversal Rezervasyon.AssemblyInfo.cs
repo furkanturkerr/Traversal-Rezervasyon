3bdca57e396a8bba22c13a43cd451518dc2f4b4c
Furkan Türker <furkanturker.dev@gmail.com>
2025-11-17 23:31:58 +0300

    Tasarımsal Düzenlemeler

--- Traversal Rezervasyon/obj/Debug/net8.0/Traversal Rezervasyon.AssemblyInfo.cs
+++ Traversal Rezervasyon/obj/Debug/net8.0/Traversal Rezervasyon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6d6670195b1b1e5e2828d2e65b02c9d33ec07b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a20e9d52aab157fd107727ce077fc3133939222a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
