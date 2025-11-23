d1b0b2c7664b58c72fd72c4be11b933db38f16d8
furkanturker <furkanturker.dev@gmail.com>
2025-11-23 19:52:31 +0300

    Menülerde Dil Desteği ve Arama İşlemi

---
+++ Traversal Rezervasyon/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,29 @@
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [Route("Member/[controller]/[action]")]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.GetAll();
+            return View(values);
+        }
+        public IActionResult GetCitiesSearchByName(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            var values = from x in destinationManager.GetAll() select x;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                values = values.Where(y => y.City.Contains(searchString));
+            }
+            return View(values.ToList());
+        }
+    }
+}
--- Traversal Rezervasyon/obj/Debug/net8.0/tr/Traversal Rezervasyon.resources.cs
+++ Traversal Rezervasyon/obj/Debug/net8.0/tr/Traversal Rezervasyon.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4037056e045bb74c20565d0f73497bfab4835de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96bcb76041aa424d9acb1aaf8cae3aebab1fc69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Traversal Rezervasyon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
